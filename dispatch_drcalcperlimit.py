9691409f183db08f3bdf41e6c82a23b786d309b0
Floris Van den Abeele <floris.vandenabeele@ugent.be>
2017-03-23 16:02:08 +0100

    Fixed bug where drop reason 3 would not get detected. Settings for drcalcper simulations.

--- dispatch_drcalcperlimit.py
+++ dispatch_drcalcperlimit.py
@@ -19,16 +19,15 @@ tracePhyTransmissions = 1
 tracePhyStates = 0
 traceMacPackets = 0
 traceMacStates = 0
-totalTime = 10 * usDataPeriod # send 10 packets on average per node
+totalTime = 100 * usDataPeriod # send 100 packets on average per node
 
 if __name__ == "__main__":
     # Create a list of cli commands that have to be run
     cli_commands = list()
-    drCalcPerLimitValues = [0.9, 0.5, 0.2, 0.1, 0.01, 0.001]
+    drCalcPerLimitValues = [0.1, 0.05, 0.025, 0.01, 0.0075, 0.005, 0.0025, 0.001]
     for drCalcPerLimit in drCalcPerLimitValues:
-        # Run nRuns for 1000*k end devices
-        for k in reversed(range (1, 10 + 1)):
-            nEndDevices = 1000*k
+        for k in [1, 5, 10]:
+            nEndDevices = 1000*k # Run nRuns for 1000*k end devices
             randomSeed = randomSeedBase + (k-1)*nRuns
             outputFileNamePrefix = "simulations/output/drcalcper/LoRaWAN-drcalcper-{}-{}".format (drCalcPerLimit, nEndDevices) # note: relative to ns-3 root folder
 
--- parse_phytx_trace.py
+++ parse_phytx_trace.py
@@ -10,7 +10,7 @@ parser.add_argument('--output', default="parse_phytx_trace.csv", help='The outpu
 
 args = parser.parse_args()
 for csvfilename in args.csvfiles:
-    print("\nParsing phy tx csv file {}".format(csvfilename))
+    print("Parsing phy tx csv file {}".format(csvfilename))
     nodes = {}
     app_packets = {}
     phy_transmissions = {}
@@ -21,29 +21,33 @@ for csvfilename in args.csvfiles:
         # Process CSV file: populate phy_transmissions data structure
         for row in linereader:
             # parse app layer packets:
-            key = row[7] # packet in hex
-            if key not in app_packets:
-                app_packets[key] = {'PhyTxBegin': [], 'PhyRxBegin': [], 'PhyTxEnd': [], 'PhyRxEnd': [], 'PhyRxDrop': [], 'PhyTxDrop': []}
+            app_packets_key = row[7] # packet in hex
+            if app_packets_key not in app_packets:
+                app_packets[app_packets_key] = {'PhyTxBegin': [], 'PhyRxBegin': [], 'PhyTxEnd': [], 'PhyRxEnd': [], 'PhyRxDrop': [], 'PhyTxDrop': []}
 
-            node_id = row[2]
+            node_id = int(row[2])
             trace_source = row[5]
-            app_packets[key][trace_source].append(node_id)
+            app_packets[app_packets_key][trace_source].append(node_id)
             if node_id not in nodes:
                 nodes[node_id] = {'DeviceType': int(row[1]), 'TransmissionsSent': 0, 'TransmissionsDelivered': 0, 'TransmissionsNotDelivered': 0, 'DropRxReason': []}
 
             # parse phy layer transmissions
-            key = row[6] # PhyTraceIdTag
-            if key not in phy_transmissions:
-                phy_transmissions[key] = {'PhyTxBegin': [], 'PhyRxBegin': [], 'PhyTxEnd': [], 'PhyRxEnd': [], 'PhyRxDrop': [], 'PhyTxDrop': [], 'Delivered': False}
+            phy_key = row[6] # PhyTraceIdTag
+            if phy_key not in phy_transmissions:
+                phy_transmissions[phy_key] = {'PhyTxBegin': [], 'PhyTxBeginMisc': [], 'PhyRxBegin': [], 'PhyTxEnd': [], 'PhyRxEnd': [], 'PhyRxDrop': [], 'PhyRxDropMisc': [], 'PhyTxDrop': [], 'Delivered': False}
+
+            phy_transmissions[phy_key][trace_source].append(node_id) # store this node under trace_source
+
+            # Store any interesting misc fields depending on trace_source:
             if trace_source == 'PhyTxBegin':
-                channel_index = row[9]
-                data_rate_index = row[10]
-                phy_transmissions[key][trace_source].append((node_id, channel_index, data_rate_index)) # store node_id and channel_index and data_rate_index as a tuple
+                channel_index = int(row[9])
+                data_rate_index = int(row[10])
+                t = (node_id, channel_index, data_rate_index) # store node_id and channel_index and data_rate_index as a tuple
+                phy_transmissions[phy_key]['PhyTxBeginMisc'].append(t)
             elif trace_source == 'PhyRxDrop':
-                drop_reason = row[9]
-                phy_transmissions[key][trace_source].append((node_id, drop_reason)) # store node_id and drop_reason as a tuple
-            else:
-                phy_transmissions[key][trace_source].append(node_id)
+                drop_reason = int(row[9])
+                t = (node_id, drop_reason) # store node_id and drop_reason as a tuple
+                phy_transmissions[phy_key]['PhyRxDropMisc'].append(t)
 
     # Process phy_transmissions data structure:
     # Was the transmission delivered and if so, was it received by a device of
@@ -59,18 +63,19 @@ for csvfilename in args.csvfiles:
             print("key={}: ERROR SKIPPING this transmissions as there is not exactly one node in PhyTxBegin list for tx = {}".format(key, tx))
             continue
 
-        tx_node_id = tx['PhyTxBegin'][0][0]
+        tx_node_id = tx['PhyTxBegin'][0] # a phy tx can only be started sending by one node
         nodes[tx_node_id]['TransmissionsSent'] += 1
         if len(tx['PhyTxEnd']) == 1 and tx['PhyTxEnd'][0] == tx_node_id:
             # check receivers
             tx_node_devicetype = nodes[tx_node_id]['DeviceType']
             expected_rx_devicetype = 0 # by default we expect the transmission was sent by an end device and will be received by a gateway
-            if tx_node_devicetype == 0: # tx was gateway
+            if tx_node_devicetype == 0: # tx was gateway, so expect an end device to receive the transmission, TODO: check specific end device?
                 expected_rx_devicetype = 1
 
-            node_received = False
-            found_expected_rx_device = False
-            receiver_in_phyrxend = False
+            node_received = False # a node started receiving the tx
+            found_expected_rx_device = False # a node that started receiving the tx has the correct device type
+            receiver_in_phyrxend = False # a node that started receiving the tx and that is the correct device type also finished receiving the transmission
+            receiver_not_in_phyrxdrop = False # a node that started receiving the tx, that is the correct device type and finished receiving the transmission also did not drop the packet
             for rx_node in tx['PhyRxBegin']:
                 node_received = True
                 # check if receiver is of opposite device type than transmitter
@@ -78,19 +83,22 @@ for csvfilename in args.csvfiles:
                     found_expected_rx_device = True
                     # Check whether this receiver reached PhyRxEnd for this transmission
                     if rx_node in tx['PhyRxEnd']:
-                        phy_transmissions[key]['Delivered'] = True
                         receiver_in_phyrxend = True
-                        number_of_delivered_transmissions += 1
-                        nodes[tx_node_id]['TransmissionsDelivered'] += 1
-                        break
+                        # Check whether this receiver did not drop the packet after ending reception
+                        if rx_node not in tx['PhyRxDrop']:
+                            receiver_not_in_phyrxdrop = True
+                            phy_transmissions[key]['Delivered'] = True
+                            number_of_delivered_transmissions += 1
+                            nodes[tx_node_id]['TransmissionsDelivered'] += 1
+                            break
 
-            transmission_not_delivered = not node_received or not found_expected_rx_device or not receiver_in_phyrxend
+            transmission_not_delivered = not node_received or not found_expected_rx_device or not receiver_in_phyrxend or not receiver_not_in_phyrxdrop
             if transmission_not_delivered:
                 number_of_undelivered_transmissions += 1
                 nodes[tx_node_id]['TransmissionsNotDelivered'] += 1
 
                 # store DropRxReason:
-                for drop_tuple in tx['PhyRxDrop']:
+                for drop_tuple in tx['PhyRxDropMisc']:
                     drop_node_id = drop_tuple [0]
                     # check if we are actually interested in the drop event (i.e. was the end device transmission dropped by a gateway phy?)
                     if nodes[drop_node_id]['DeviceType'] == expected_rx_devicetype:
@@ -100,22 +108,26 @@ for csvfilename in args.csvfiles:
                         # store drop reason over all nodes:
                         sim_drop_reasons.append(drop_reason)
                         # store data rate index for drop_reason
-                        data_rate_index = int(tx['PhyTxBegin'][0][2])
+                        data_rate_index = int(tx['PhyTxBeginMisc'][0][2]) # always index 0 here as there is only one node that starts sending a transmission
                         if drop_reason not in sim_drop_reasons_datarateindex:
                             sim_drop_reasons_datarateindex[drop_reason] = list()
                         sim_drop_reasons_datarateindex[drop_reason].append(data_rate_index)
 
                 # print reason:
                 if not node_received:
-                    pass
                     # print ("key = {}: Transmission not delivered because no node started receiving it".format(key))
+                    pass
                 else:
                     if not found_expected_rx_device:
-                        pass
-                        # print ("key = {}: Transmission not delivered because there wasn't a suitable receiver, tx_node_devicetype = {} tx = {}".format(key, tx_node_devicetype, tx))
+                        print ("key = {}: Transmission not delivered because there wasn't a suitable receiver, tx_node_devicetype = {} tx = {}".format(key, tx_node_devicetype, tx))
+                        # pass
                     else:
                         if not receiver_in_phyrxend:
                             print ("key = {}: Transmission not delivered because the receiver did not enter the PhyRxEnd state, tx = {}".format(key, tx))
+                        else:
+                            if not receiver_not_in_phyrxdrop:
+                                #print ("key = {}: Transmission not delivered because the receiver dropped the packet after reception, tx = {}".format(key, tx))
+                                pass
         else:
             if tx_node_id in tx['PhyTxDrop']:
                 print("key = {}: case where transmission is aborted is not implemented. tx = {}".format(tx)) # TODO: count number of aborted transmissions
@@ -157,7 +169,7 @@ for csvfilename in args.csvfiles:
     # specific data rate index was dropped for that drop reason
     print("\nNumber of dropped PHY transmissions at a specific data rate index for every drop reason:")
     print("Drop reason\tDR:drop_count")
-    for drop_reason in sim_drop_reasons_datarateindex:
+    for drop_reason in sorted(sim_drop_reasons_datarateindex):
         s = "0x0{} ".format(drop_reason)
         for data_rate_index in range(6):
             count = sim_drop_reasons_datarateindex[drop_reason].count(data_rate_index)
